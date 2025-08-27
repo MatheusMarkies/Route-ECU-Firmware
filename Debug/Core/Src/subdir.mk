c2eed6b8ec0725923be172d6db091a390f6984ed
Matheus Markies <48259420+MatheusMarkies@users.noreply.github.com>
2025-08-27 20:18:24 -0300

    Added: Initial UART Communication Protocol, to be used in conjunction with the desktop program.

--- Debug/Core/Src/subdir.mk
+++ Debug/Core/Src/subdir.mk
@@ -5,6 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Core/Src/communication_serial_protocol.c \
 ../Core/Src/crank_decoder.c \
 ../Core/Src/engine_control.c \
 ../Core/Src/engine_sensors.c \
@@ -19,6 +20,7 @@ C_SRCS += \
 ../Core/Src/system_stm32h7xx.c 
 
 OBJS += \
+./Core/Src/communication_serial_protocol.o \
 ./Core/Src/crank_decoder.o \
 ./Core/Src/engine_control.o \
 ./Core/Src/engine_sensors.o \
@@ -33,6 +35,7 @@ OBJS += \
 ./Core/Src/system_stm32h7xx.o 
 
 C_DEPS += \
+./Core/Src/communication_serial_protocol.d \
 ./Core/Src/crank_decoder.d \
 ./Core/Src/engine_control.d \
 ./Core/Src/engine_sensors.d \
@@ -54,7 +57,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/crank_decoder.cyclo ./Core/Src/crank_decoder.d ./Core/Src/crank_decoder.o ./Core/Src/crank_decoder.su ./Core/Src/engine_control.cyclo ./Core/Src/engine_control.d ./Core/Src/engine_control.o ./Core/Src/engine_control.su ./Core/Src/engine_sensors.cyclo ./Core/Src/engine_sensors.d ./Core/Src/engine_sensors.o ./Core/Src/engine_sensors.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/ignition_driver.cyclo ./Core/Src/ignition_driver.d ./Core/Src/ignition_driver.o ./Core/Src/ignition_driver.su ./Core/Src/injector_driver.cyclo ./Core/Src/injector_driver.d ./Core/Src/injector_driver.o ./Core/Src/injector_driver.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32h7xx_hal_msp.cyclo ./Core/Src/stm32h7xx_hal_msp.d ./Core/Src/stm32h7xx_hal_msp.o ./Core/Src/stm32h7xx_hal_msp.su ./Core/Src/stm32h7xx_it.cyclo ./Core/Src/stm32h7xx_it.d ./Core/Src/stm32h7xx_it.o ./Core/Src/stm32h7xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32h7xx.cyclo ./Core/Src/system_stm32h7xx.d ./Core/Src/system_stm32h7xx.o ./Core/Src/system_stm32h7xx.su
+	-$(RM) ./Core/Src/communication_serial_protocol.cyclo ./Core/Src/communication_serial_protocol.d ./Core/Src/communication_serial_protocol.o ./Core/Src/communication_serial_protocol.su ./Core/Src/crank_decoder.cyclo ./Core/Src/crank_decoder.d ./Core/Src/crank_decoder.o ./Core/Src/crank_decoder.su ./Core/Src/engine_control.cyclo ./Core/Src/engine_control.d ./Core/Src/engine_control.o ./Core/Src/engine_control.su ./Core/Src/engine_sensors.cyclo ./Core/Src/engine_sensors.d ./Core/Src/engine_sensors.o ./Core/Src/engine_sensors.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/ignition_driver.cyclo ./Core/Src/ignition_driver.d ./Core/Src/ignition_driver.o ./Core/Src/ignition_driver.su ./Core/Src/injector_driver.cyclo ./Core/Src/injector_driver.d ./Core/Src/injector_driver.o ./Core/Src/injector_driver.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32h7xx_hal_msp.cyclo ./Core/Src/stm32h7xx_hal_msp.d ./Core/Src/stm32h7xx_hal_msp.o ./Core/Src/stm32h7xx_hal_msp.su ./Core/Src/stm32h7xx_it.cyclo ./Core/Src/stm32h7xx_it.d ./Core/Src/stm32h7xx_it.o ./Core/Src/stm32h7xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32h7xx.cyclo ./Core/Src/system_stm32h7xx.d ./Core/Src/system_stm32h7xx.o ./Core/Src/system_stm32h7xx.su
 
 .PHONY: clean-Core-2f-Src
 
